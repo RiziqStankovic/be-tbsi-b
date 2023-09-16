6671f09d928c87ed5a4658f95a218301912d0488
ramyabyyu <bintantramy@gmail.com>
2023-09-16 09:33:55 +0700

    modifying listen port instance and mongo init to run in a serverless

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const { default: mongoose } = require('mongoose');
 const app = require('./src/server');
-const mongoInit = require('./src/config/database_connect');
 require('dotenv').config();
-
-mongoInit();
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log('Server at http://localhost:' + port);
-});
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Mongodb ready');
+        app.listen(port, () => {
+            console.log('Server at http://localhost:' + port);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
