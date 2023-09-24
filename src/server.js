320698d4ee82a2b84beeba5a8ec9f373e39670b0
ramyabyyu <bintantramy@gmail.com>
2023-09-24 13:57:40 +0700

    change port for cors origin

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.use('/api', routes);
 
